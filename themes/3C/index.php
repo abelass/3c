8f7fcf3aac00e45b87f19aa1363845ce2103f67e
abelass <websolutions@mychacra.net>
2012-11-20 13:13:18 +0100

    permettre l'affichage correcte des catégories, index original

--- themes/3C/index.php
+++ themes/3C/index.php
@@ -1,21 +1,36 @@
-<?php get_header(); 
- if(get_query_var('post_type')!='entreprise'){
-?>	
-	<div id="content" class="clearfix">
-		<div id="content-area"<?php if ( isset($fullWidthPage) && $fullWidthPage ) echo(' class="fullwidth_home"');?>>
-			<div class="entry clearfix">
-				<?php get_template_part('includes/entry'); ?>
-			</div> <!-- end .entry -->
+<?php if (is_archive()) $post_number = get_option('minimal_archivenum_posts');
+if (is_search()) $post_number = get_option('minimal_searchnum_posts');
+if (is_tag()) $post_number = get_option('minimal_tagnum_posts');
+if (is_category()) $post_number = get_option('minimal_catnum_posts'); ?>
+<?php get_header(); ?>
+    
+    <?php global $query_string; 
+    if (is_category()) query_posts($query_string . "&showposts=$post_number&paged=$paged&cat=$cat");
+    else query_posts($query_string . "&showposts=$post_number&paged=$paged"); ?>
+        
+    <div id="content" class="clearfix">
+        <div id="content-area"<?php if ( isset($fullWidthPage) && $fullWidthPage ) echo(' class="fullwidth_home"');?>>
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            
+                <div class="entry clearfix">
+                    <?php include(TEMPLATEPATH . '/includes/entry.php'); ?>
+                </div> <!-- end .entry -->
+                
+            <?php endwhile; ?>
+                
+                <?php if(function_exists('wp_pagenavi')) { wp_pagenavi(); }
+                    else { ?>   
+                            <?php include(TEMPLATEPATH . '/includes/navigation.php'); ?>
 
-		</div> <!-- end #content-area -->	
-			<?php get_sidebar(); ?>	
+                <?php } ?>
 
-	</div> <!-- end #content --> 
+            <?php else : ?>
+                <?php include(TEMPLATEPATH . '/includes/no-results.php'); ?>
+            <?php endif; wp_reset_query(); ?>
 
-<?php 
-}
-else{
-	$type_entreprise='';
-	include(locate_template('taxonomy.php'));
-	}
-get_footer(); ?>	
+        </div> <!-- end #content-area -->   
+    
+<?php get_sidebar(); ?>
+    </div> <!-- end #content --> 
+
+<?php get_footer(); ?>  
