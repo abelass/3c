cdd973d3b56fff58db858693b1a69cced7589ad6
abelass <websolutions@mychacra.net>
2013-02-28 17:43:50 +0100

    maj depuis serveur

--- themes/3C/searchform.php
+++ themes/3C/searchform.php
@@ -3,8 +3,6 @@
 	$s=$_GET['src']?$_GET['src']:$_GET['s'];
 ?>
 	
-
-	
 	<form method="get" id="searchform" action="<?php echo esc_url(  get_permalink( 67) ); ?>">
 	<input type="hidden" name="post_type" value="entreprise" />
 	<input type="hidden"  name="type_entreprise"  value="<?php echo $type_entreprise ?>" value="<?php the_search_query() ?>"/>
--- themes/3C/test.php
+++ themes/3C/test.php
@@ -1,12 +1,14 @@
 <?php 
 /*
-Template Name: Member Page
+Template Name: flush Page
 */
 
 //Ensure the $wp_rewrite global is loaded
 global $wp_rewrite;
 //Call flush_rules() as a method of the $wp_rewrite object
 $wp_rewrite->flush_rules();
+
+echo '(ok)';
 ?>
  
 
